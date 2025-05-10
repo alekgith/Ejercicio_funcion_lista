9a3bc5fe0909f08e01df34663d2cf438fa094a9b
alekgith <alekmoritautn@gmail.com>
2025-05-10 13:51:00 -0300

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from manejo_de_listas import*
+import manejo_de_listas.agregar as lista_agregar
+import manejo_de_listas.insertar as lista_insertar
+import manejo_de_listas.obtener_indice as lista_indice
+import manejo_de_listas.eliminar as lista_eliminar_1
+import manejo_de_listas.eliminar_primer_instancia as lista_eliminar_2
+import manejo_de_listas.eliminar_todos as lista_eliminar_3
+import manejo_de_listas.vaciar_lista as lista_vaciar
+import manejo_de_listas.validar as validacion
             
 a = input("Introduzca el tamaño de la lista:\n")
 
-a = validar(a)
+a = validacion.validar(a)
 
 lista = [None]*a
 
@@ -15,7 +22,7 @@ print(f"Lista: {lista}")
 
 c = input("Agrega un elemento al final: \n")
 
-agregar(lista,c)
+lista_agregar.agregar(lista,c)
 
 print(lista)
 
@@ -23,15 +30,15 @@ d = input("Inserta un elemento: \n")
 
 indice = input("Elija la posición en la que quiera agregar el elemento\n")
 
-indice = validar(indice)
+indice = validacion.validar(indice)
 
-insertar(lista,d,indice)
+lista_insertar.insertar(lista,d,indice)
 
 print(lista)
 
 element = input("Escriba un elemento para obtener su primera posición\n")
 
-f = obtener_indice(lista,element)
+f = lista_indice.obtener_indice(lista,element)
 
 if f > -1:
     print(f"La primer posición de {element} se encuentra en {f}\n")
@@ -40,7 +47,7 @@ else:
 
 print("Eliminando el último elemento de la lista")
 
-eliminar(lista)
+lista_eliminar_1.eliminar(lista)
 
 print(f"{lista}\n")
 
@@ -48,7 +55,7 @@ element= input("Escriba un elemento para borrar su primer ocurrencia\n")
 
 print("Eliminando la primer ocurrencia de un elemento de la lista")
 
-eliminar_primer_instancia(lista,element)
+lista_eliminar_2.eliminar_primer_instancia(lista,element)
 
 print(lista)
 
@@ -56,11 +63,11 @@ element = input("Escriba un elemento para borrar todas sus ocurrencias\n")
 
 print(f"Eliminando todas las ocurrencias de {element}")
 
-eliminar_todos(lista, element)
+lista_eliminar_3.eliminar_todos(lista, element)
 
 print(lista)
 
-vaciar_lista(lista)
+lista_vaciar.vaciar_lista(lista)
 
 print("Vaciando la lista")
 
